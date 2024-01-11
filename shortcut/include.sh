abe85ba104ab2fb29ac08d1623b1f2897036d2e3
Ata <quetzalskeith@gmail.com>
2024-01-11 09:41:41 +0700

    Added copy stubs from shortcut

--- shortcut/include.sh
+++ shortcut/include.sh
@@ -5,6 +5,9 @@ FPM_PATH="$SCRIPT_PATH/php-fpm.d"
 
 SUPERVISOR_CONF_PATH="$SCRIPT_PATH/supervisor/conf.d"
 
+STUBS_PATH="$SCRIPT_PATH/stubs"
+COMPOSE_PATH="$SCRIPT_PATH/$ENV-confs"
+
 if [[ -d "$PHP_PATH" && ! -f "$PHP_PATH/php.ini" ]]
 then
     phpini="php.ini-production"
@@ -33,6 +36,21 @@ then
             cp $template $config
         fi
     done
+fi
+
+if [[ ! -d "$COMPOSE_PATH" ]]
+then
+    mkdir "$COMPOSE_PATH"
+
+    if [[ -d "$STUBS_PATH" ]]
+    then
+        for stub in $(find $STUBS_PATH -type f -regex ".*\.stub")
+        do
+            compose="$(echo $(basename $stub) | sed 's/\.stub/\.yaml/g')"
+
+            cp "$stub" "$COMPOSE_PATH/$compose"
+        done
+    fi
 fi
 
 $SCRIPT_PATH/../vendor/bin/rundock $ENV ${@}
